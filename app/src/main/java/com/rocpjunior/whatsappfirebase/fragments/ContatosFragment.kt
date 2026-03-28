4b767fe5c1af5e2a8a6a803989800892ee3c8fca
rocpjunior <jugatenho@hotmail.com>
2026-03-27 21:05:58 -0300

    adicionando o firebase e funcoes na aba de Contatos

--- app/src/main/java/com/rocpjunior/whatsappfirebase/fragments/ContatosFragment.kt
+++ app/src/main/java/com/rocpjunior/whatsappfirebase/fragments/ContatosFragment.kt
@@ -1,19 +1,70 @@
 package com.rocpjunior.whatsappfirebase.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.rocpjunior.whatsappfirebase.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.QuerySnapshot
+import com.rocpjunior.whatsappfirebase.databinding.FragmentContatosBinding
+import com.rocpjunior.whatsappfirebase.model.Usuario
 
 
 class ContatosFragment : Fragment() {
+
+    private lateinit var binding: FragmentContatosBinding
+    private lateinit var eventSnapshot: ListenerRegistration
+
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+    private val firestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false)
+        binding = FragmentContatosBinding.inflate(
+            inflater, container, false
+        )
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        listenerContatos()
+    }
+
+    private fun listenerContatos() {
+        eventSnapshot = firestore
+            .collection("Usuarios")
+            .addSnapshotListener { querySnapshot, erro ->
+
+                val listaContatos = mutableListOf<Usuario>()
+                val documentos = querySnapshot?.documents
+                documentos?.forEach { documentSnapshot ->
+
+                    val usuario = documentSnapshot.toObject(Usuario::class.java)
+                    if(usuario != null) {
+                        val idUsuarioLogado = firebaseAuth.currentUser?.uid
+                        if(idUsuarioLogado != null){
+                            if(idUsuarioLogado != usuario.id){
+                                listaContatos.add(usuario)
+                            }
+                        }
+                    }
+                }
+            }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        eventSnapshot.remove()
     }
 }
--- app/src/main/java/com/rocpjunior/whatsappfirebase/model/Usuario.kt
+++ app/src/main/java/com/rocpjunior/whatsappfirebase/model/Usuario.kt
@@ -1,8 +1,8 @@
 package com.rocpjunior.whatsappfirebase.model
 
 data class Usuario(
-    var id: String,
-    var nome: String,
-    var email: String,
+    var id: String = "",
+    var nome: String = "",
+    var email: String = "",
     var foto: String = ""
 )
